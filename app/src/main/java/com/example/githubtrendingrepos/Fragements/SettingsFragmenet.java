ada8e7e6fc7139a218fc31a882aa742970470d33
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-05-31 18:56:23 +0000

    adding settings fragements

---
+++ app/src/main/java/com/example/githubtrendingrepos/Fragements/SettingsFragmenet.java
@@ -0,0 +1,42 @@
+package com.example.githubtrendingrepos.Fragements;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.githubtrendingrepos.Activities.MainActivity;
+import com.example.githubtrendingrepos.R;
+
+public class SettingsFragmenet extends Fragment {
+
+    MainActivity activity;
+
+    private static final String TAG = "SettingsFragmenet";
+
+    public static SettingsFragmenet newInstance() {
+        SettingsFragmenet fragment = new SettingsFragmenet();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i(TAG, "Lunched");
+        activity = (MainActivity) getActivity();
+    }
+
+
+}
